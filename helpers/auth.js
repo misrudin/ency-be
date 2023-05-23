2832104b388a1f1a9aa8c74076bcd1df52ab082e
misrudin <misrudinz@gmail.com>
2023-05-23 21:14:28 +0700

    test

--- helpers/auth.js
+++ helpers/auth.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Middleware untuk verifikasi token
 const verifyAuth = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
